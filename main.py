fb8a046e6f5c280b973bc40b60add43fde75b6de
Bogdan <bogdan@workbook.local>
2026-04-11 01:11:41 +0300

    ch3l3: moving around

--- main.py
+++ main.py
@@ -1,25 +1,32 @@
 import pygame
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state
+from player import Player
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f'Screen width: {SCREEN_WIDTH}')
     print(f'Screen height: {SCREEN_HEIGHT}')
+
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     game_clock = pygame.time.Clock()
     dt = 0
+
+    ship = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     #start game loop
     while True:
         log_state()
         for event in pygame.event.get():
-            if event == pygame.QUIT:
+            if event.type == pygame.QUIT:
                 return
         dt = game_clock.tick(60) / 1000
         # print(dt)
 
         screen.fill('black')
+        ship.draw(screen)
+        ship.update(dt)
         pygame.display.flip()
 
 if __name__ == "__main__":
